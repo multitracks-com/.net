c0030783237c5c78b5a6166dece4bb1a79d7ef00
Sai Abhinav Sirimamilla <navabhi29.s2@gmail.com>
2023-05-21 16:11:23 +0530

    Interview Process

---
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE dbo.GetArtistDetails
+	@artistID INT
+AS
+BEGIN
+	SELECT *
+	FROM dbo.Artist Artist
+	LEFT JOIN dbo.Song Song on Artist.artistID = Song.artistID
+	LEFT JOIN dbo.Album Album on Album.albumID = Song.albumID
+	WHERE Artist.artistID = @artistID
+END
---
+++ WebAPI/API/Controllers/ArtistController.cs
@@ -0,0 +1,77 @@
+﻿using API.Models;
+using DataAccess;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public partial class ArtistController : ApiController
+    {
+        /// <summary>
+        /// this API can be accessed using this -"api.multitracks.com/artist/search?name={name}"
+        /// replace {name} with title.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>IHttpActionResult</returns>
+        [HttpGet]
+        [Route("api.multitracks.com/artist/search")]
+        public IHttpActionResult Search(string name)
+        {
+            Artist artist = new Artist();
+            List<Artist> artists = artist.GetArtists();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    return BadRequest("The name parameter is required.");
+                }
+
+                var matchedArtists = artists.Where(a => a.title.ToLower().Contains(name.ToLower())).ToList();
+
+                if (matchedArtists.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(matchedArtists);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An exception occurred while searching for an artist.");
+                return InternalServerError(ex);
+            }
+        }
+        /// <summary>
+        /// this Post Method is used to add the new Artist to the Artist Table
+        /// </summary>
+        /// <param name="artist"></param>
+        /// <returns>IHttpActionResult</returns>
+        [HttpPost]
+        [Route("api.multitracks.com/artist/add")]
+        public IHttpActionResult AddArtist(Artist artist)
+        {
+            try
+            {
+                Artist newArtist = new Artist();
+                if (artist == null)
+                {
+                    return BadRequest("Invalid artist data. Artist data cannot be null.");
+                }
+                int id = newArtist.SetArtist(artist);
+                return Ok("Artist Added Succesfully");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,"The Artist you are trying to add is Invalid");
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
