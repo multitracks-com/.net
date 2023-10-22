cf7328031263bddcd20a0d01a9d4c8cbf6a20959
John Kehinde <kehindejohn018@gmail.com>
2023-10-22 08:45:33 +0100

    added stored procedure for GetArtistDetails and GetArtistDetailsByName

---
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetArtistDetails]
+	@artistID INT
+AS
+BEGIN
+	SELECT Artist.title AS artistTitle,
+		   Artist.imageURL AS artistImageURL,
+		   Artist.heroURL AS artistHeroURL,
+		   Artist.biography AS artistBiography,
+		   Song.title AS songTitle,
+		   Song.bpm AS songBPM,
+		   Album.title AS albumTitle,
+		   Album.imageURL AS albumImageURL
+	FROM dbo.Artist Artist
+	LEFT JOIN dbo.Song Song ON Artist.artistID = Song.artistID
+	LEFT JOIN dbo.Album Album ON Album.albumID = Song.albumID
+	WHERE Artist.artistID = @artistID
+END
